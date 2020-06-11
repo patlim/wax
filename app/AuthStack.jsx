1df38c8a0e9bff93620b069c0e04e4890aa05868
Pat-Lim <impatlim@gmail.com>
2020-06-11 19:52:31 +1200

    fix login/signup screens

--- app/AuthStack.jsx
+++ app/AuthStack.jsx
@@ -6,6 +6,39 @@ import { AuthContext } from "./AuthProvider"
 
 const Stack = createStackNavigator()
 
+function Login({ navigation }) {
+  const { login } = useContext(AuthContext)
+  return (
+    <View style={styles.center}>
+      <Text>I am a login screen</Text>
+      <Button
+        title="log me in"
+        onPress={() => {
+          login()
+        }}
+      />
+      <Button
+        title="go to register"
+        onPress={() => {
+          navigation.navigate("SignUp")
+        }}
+      />
+    </View>
+  )
+}
+
+const SignUp = ({ navigation }) => (
+  <View style={styles.center}>
+    <Text>I am a Register screen</Text>
+    <Button
+      title="go to login"
+      onPress={() => {
+        navigation.navigate("Login")
+      }}
+    />
+  </View>
+)
+
 export const AuthStack = ({}) => {
   return (
     <Stack.Navigator
@@ -16,7 +49,6 @@ export const AuthStack = ({}) => {
     >
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen options={{ headerTitle: "Sign Up" }} name="SignUp" component={SignUp} />
-      <Stack.Screen name="Loading" component={Loading} />
     </Stack.Navigator>
   )
 }
